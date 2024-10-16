a619e65dce6c96ca3deec9d651655535a1fdad27
NabilRaiyan <rraiyan77@gmail.com>
2024-10-16 12:18:22 +0600

    creating customizer page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -13,10 +13,52 @@ import { fadeAnimation, slideAnimation } from '../config/motion'
 import { AiPicker, ColorPicker, Tab, CustomButton, FilePicker } from '../components'
 
 const Customizer = () => {
+
+    const snap = useSnapshot(state)
+
     return (
-        <div>
-        Customizer
-        </div>
+        <AnimatePresence>
+            {!snap.intro && (
+                <>
+                    <motion.div key='custom' className='z-10 top-0 absolute left-0' {...slideAnimation('left')}>
+                        <div className='flex items-center min-h-screen'>
+                            <div className='editortabs-container tabs'>
+                                {
+                                    EditorTabs.map((tab)=>(
+                                        <Tab key={tab.name}
+                                            tab={tab}
+                                            handleClick = {()=>{}}
+                                        />
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </motion.div>
+
+                    <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+                        <CustomButton type='filled' 
+                                       title="Go Back" 
+                                       handleClick={() => state.intro = true}
+                                       customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
+                    </motion.div>
+
+
+                    <motion.div className='filtertabs-container' {...slideAnimation('up')}>
+                                {
+                                    FilterTabs.map((tab)=>(
+                                        <Tab key={tab.name}
+                                            tab={tab}
+                                            isFilterTab
+                                            isActiveTab=""
+                                            handleClick = {()=>{}}
+                                        />
+                                    ))
+                                }     
+                    </motion.div>
+                </>
+            )}
+        </AnimatePresence>
     )
 }
 
