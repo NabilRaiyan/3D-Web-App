5a7aa013f416ddc0c5bf94842120e9adf11e463e
NabilRaiyan <rraiyan77@gmail.com>
2024-10-16 12:08:38 +0600

    creating home page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import CustomButton from "./CustomButton";
 import Tab from './Tab'
 import ColorPicker from "./ColorPicker";
 import FilePicker from './FilePicker'
+import AiPicker from './AiPicker'
 
-export {CustomButton, ColorPicker, Tab, FilePicker}
+export {CustomButton, ColorPicker, Tab, FilePicker, AiPicker}
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
+
+import { AnimatePresence, motion } from 'framer-motion'
+import config from '../config/config'
+import {download} from '../assets'
+import { downloadCanvasToImage, reader} from '../config/helpers'
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
+import { fadeAnimation, slideAnimation } from '../config/motion'
+
+import { AiPicker, ColorPicker, Tab, CustomButton, FilePicker } from '../components'
 
 const Customizer = () => {
     return (
