3b44b26fbad20ec68f0359743eb40b997893ece4
NabilRaiyan <rraiyan77@gmail.com>
2024-10-16 11:20:07 +0600

    creating projects

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+    <p className='m-5 text-xl font-serif bg-gray-200 p-4'>Nma ei s</p>
+     <h1 className='text-2xl bg-cyan-300 p-4 rounded'>Hello my name is raiyan</h1>
+    </>
+  )
+}
+
+export default App
