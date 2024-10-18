0fd0d177d3b598f387042a080b9965b9353c02f0
NabilRaiyan <rraiyan77@gmail.com>
2024-10-18 18:06:44 +0600

    creating Shirt backdrop shadows and color

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -4,14 +4,14 @@ import Shirt from './Shirt'
 import Backdrop from './Backdrop'
 import CameraRig from './CameraRig'
 
-// creating canvas
+// creating canvas model
 const CanvasModel = () => {
   return (
     <Canvas shadows camera={{position: [0, 0, 0], fov: 25}} 
             gl={{preserveDrawingBuffer: true}}
             className='w-full max-w-full h-full transition-all ease-in'
     >
-      <ambientLight intensity={0.5}/>
+      <ambientLight intensity={0.5} />
       <Environment preset='city'/>
 
       {/* Creating camera rig */}
